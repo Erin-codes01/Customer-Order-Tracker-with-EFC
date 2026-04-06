0fdbf672daee4d5041540ec66e301f180c521d7a
Erin Shea <e.l.shea23@gmail.com>
2026-04-05 21:28:44 -0500

    removed duplicated folder

--- TrackerContext.cs
+++ TrackerContext.cs
@@ -2,25 +2,27 @@ using Microsoft.EntityFrameworkCore;
 
 public class TrackerContext : DbContext
 {
-    //Create a public DbSet for Customers and Orders. Set them by type
+    // DbSets for your entities
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=CustomerOrders.db");
 
-	//Create a protected override void to set the Data Source to CustomerOrders.db
-
-	//Use this potected override void for OnModelCreating
-	//Pass in a ModelBuilder
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        //Use the ModelBuilder.Entity to ensure Customer has a unique email
-		
+        modelBuilder.Entity<Customer>()
+            .HasIndex(c => c.Email)
+            .IsUnique();
 
-		//Use the ModelBuilder.Entity to set a One to Many relationship between customer and orders
-		//Enforce the Foreign Key with HasForeignKey
-		//Use OnDelete to cascade delete orders for the deleted customer
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.Customer)
+            .WithMany(c => c.Orders)
+            .HasForeignKey(o => o.CustomerId)
+            .OnDelete(DeleteBehavior.Cascade);
 
-
-		//Use ModelBuilder.Entity to ensure that Order has a Total Amount
-		//Set Precision to (18, 2)
-
+        modelBuilder.Entity<Order>()
+            .Property(o => o.TotalAmount)
+            .HasPrecision(18, 2);
     }
 }
